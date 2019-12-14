b38e6f8fb5cdb04f3b3a4af4865a72cd0b78fa5e
alekseychenko <oleksiychenko@gmail.com>
2019-12-14 14:17:38 +0200

    .....

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Button btnsingin,reg;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        reg = findViewById(R.id.reg);
+
+
+
+    }
+
+}
